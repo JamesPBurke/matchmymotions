a0ca1e2f6da0e54944d4779476f0643615de8574
JamesPBurke <burkej@sbregional.org>
2020-08-03 15:14:41 -0400

    first part of connecting completed

--- main.ts
+++ main.ts
@@ -1,15 +1,34 @@
 input.onButtonPressed(Button.AB, function () {
-    radio.sendValue("Player", 0)
-    Player = 1
+    radio.sendValue("Role", 2)
+    Role = 1
 })
 radio.onReceivedValue(function (name, value) {
-    if (name == "Player") {
-        Player = value
-        basic.showString("Hello!")
+    if (name == "Role") {
+        Role = value
+        if (value == 1) {
+            Rolename = "Leader"
+        } else {
+            Rolename = "Follower"
+        }
+        basic.showString("Role is " + Rolename)
     }
 })
-let Player = 0
+let Rolename = ""
+let Role = 0
+Role = 0
 radio.setGroup(191)
+while (Role == 0) {
+    basic.showLeds(`
+        . . # # .
+        . # . . #
+        . # . . #
+        . . . . #
+        . . . # .
+        `)
+    basic.clearScreen()
+    basic.pause(100)
+}
+basic.showIcon(IconNames.StickFigure)
 basic.forever(function () {
 	
 })
